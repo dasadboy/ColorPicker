6460abc2f29618a69e2f7e596122e99be31706f9
dasadboy <42483448+dasadboy@users.noreply.github.com>
2019-05-24 00:07:42 -0400

    Implemented rendering functions for TwoBarPicker.

--- colorpicker.js
+++ colorpicker.js
@@ -1,9 +1,11 @@
-const $create = document.createElement.bind(document);
+const $ = document.querySelector.bind(document)
+      $create = document.createElement.bind(document);
 
 const $createCanv = (height, width) => {
-  const canv = $create("canv");
+  const canv = $create("canvas");
   canv.height = height;
   canv.width = width;
+  return canv;
 }
 
 class ColorPicker {
@@ -13,5 +15,43 @@ class ColorPicker {
     } else {
       this.pointer = null
     }
+  }
+}
+
+class TwoBarPicker extends ColorPicker {
+  constructor(pointer=null, height=null, width=null, horizontal=null) {
+    super(pointer);
+    // Picks hue
+    this.hue = TwoBarPicker.renderHueBar(height, width, !!horizontal);
+    // Picks brightness
+    this.brightness = TwoBarPicker.renderBrightnessBar(width, height, horizontal);
+  }
+  static renderHueBar(h, w, hor) {
+    const height = h || 512,
+          width = w || 50;
+    const canv = $createCanv(height, width),
+          ctx = canv.getContext("2d"),
+          grad = ctx.createLinearGradient(0, 0, width * hor, height * !hor),
+          colors = ["#f00", "#ff0", "#0f0", "#0ff", "#00f", "#f0f", "#f00"];
+    console.log(height * !hor);
+    for (let i in colors) {
+      grad.addColorStop(i/6, colors[i]);
+    }
+    ctx.fillStyle = grad;
+    ctx.fillRect(0, 0, width, height);
+    return canv;
+  }
+  static renderBrightnessBar(w, h, hor, hue) {
+    const height = h || 512,
+          width = w || 50;
+    const canv = $createCanv(height, width),
+          ctx = canv.getContext("2d"),
+          grad = ctx.createLinearGradient(0, 0, width * hor, height * !hor);
+    grad.addColorStop(0, "#fff");
+    grad.addColorStop(0.5, "#f00");
+    grad.addColorStop(1, "#000");
+    ctx.fillStyle = grad;
+    ctx.fillRect(0, 0, width, height);
+    return canv;
   }
 }
