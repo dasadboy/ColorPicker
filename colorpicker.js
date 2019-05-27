b54203d344cf63d897762891403dfd5141ed30d5
dasadboy <42483448+dasadboy@users.noreply.github.com>
2019-05-26 20:31:50 -0400

    Created 3 bar colorpicker. Fixed bug that occurs with scrolled div.

--- colorpicker.js
+++ colorpicker.js
@@ -31,7 +31,6 @@ class TwoBarPicker extends ColorPicker {
           ctx = canv.getContext("2d"),
           grad = ctx.createLinearGradient(0, 0, w * !!hor, h * !hor),
           colors = ["#f00", "#ff0", "#0f0", "#0ff", "#00f", "#f0f", "#f00"];
-    console.log(0, 0, w * !!hor, h * !hor)
     const canvas = {
       elem: canv,
       ctx: ctx,
@@ -82,7 +81,7 @@ class TwoBarPicker extends ColorPicker {
     const getPosX = x => {
       let current = canvas;
       while (!!current) {
-        x -= (current.offsetLeft + current.scrollLeft);
+        x -= current.offsetLeft - current.scrollLeft;
         current = current.parentElement;
       }
       return x;
@@ -91,7 +90,7 @@ class TwoBarPicker extends ColorPicker {
     const getPosY = y => {
       let current = canvas;
       while (!!current) {
-        y -= (current.offsetLeft + current.scrollLeft);
+        y -= current.offsetTop - current.scrollTop;
         current = current.parentElement;
       }
       return y;
@@ -165,8 +164,8 @@ class TwoBarPicker extends ColorPicker {
 
     const getPosX = x => {
       let current = canvas;
-      while (!!current) {
-        x -= (current.offsetLeft + current.scrollLeft);
+      while (!!current.parentElement) {
+        x -= current.offsetLeft - current.scrollLeft;
         current = current.parentElement;
       }
       return x;
@@ -174,8 +173,8 @@ class TwoBarPicker extends ColorPicker {
 
     const getPosY = y => {
       let current = canvas;
-      while (!!current) {
-        y -= (current.offsetLeft + current.scrollLeft);
+      while (!!current.parentElement) {
+        y -= current.offsetTop - current.scrollTop;
         current = current.parentElement;
       }
       return y;
@@ -224,7 +223,167 @@ class TwoBarPicker extends ColorPicker {
 }
 
 class ThreeBarPicker extends ColorPicker {
-  constructor() {
+  constructor(width=null, height=null, horizontal=null) {
+    super()
+    this.hor = !!horizontal;
+    if (!this.hor) {
+      this.width = width || 50;
+      this.height = height || 512;
+    } else {
+      this.width = width || 512;
+      this.height = height || 50;
+    }
+    this.red = ThreeBarPicker.renderRed(this.width, this.height, this.hor);
+    this.blue = ThreeBarPicker.renderBlue(this.width, this.height, this.hor);
+    this.green = ThreeBarPicker.renderGreen(this.width, this.height, this.hor);
+  }
+  static renderRed(w, h, hor) {
+    const canv = $createCanv(w, h),
+          ctx = canv.getContext("2d"),
+          grad = ctx.createLinearGradient(0, 0, w * !!hor, h * !hor),
+          colors = ["#f00", "#000"];
+    const canvas = {
+      elem: canv,
+      ctx: ctx,
+      pos: {
+        x: 0,
+        y: 0
+      },
+      val: "ff"
+    };
+    for (let i in colors) {
+      grad.addColorStop(i*0.98 + 0.01, colors[i]);
+    }
+    canvas.ctx.fillStyle = grad;
+    canvas.ctx.fillRect(0, 0, w, h);
+    return canvas;
+  }
+  static renderBlue(w, h, hor) {
+    const canv = $createCanv(w, h),
+          ctx = canv.getContext("2d"),
+          grad = ctx.createLinearGradient(0, 0, w * !!hor, h * !hor),
+          colors = ["#00f", "#000"];
+    const canvas = {
+      elem: canv,
+      ctx: ctx,
+      pos: {
+        x: 0,
+        y: 0
+      },
+      val: "ff"
+    };
+    for (let i in colors) {
+      grad.addColorStop(i*0.98 + 0.02, colors[i]);
+    }
+    canvas.ctx.fillStyle = grad;
+    canvas.ctx.fillRect(0, 0, w, h);
+    return canvas;
+  }
+  static renderGreen(w, h, hor) {
+    const canv = $createCanv(w, h),
+          ctx = canv.getContext("2d"),
+          grad = ctx.createLinearGradient(0, 0, w * !!hor, h * !hor),
+          colors = ["#0f0", "#000"];
+    const canvas = {
+      elem: canv,
+      ctx: ctx,
+      pos: {
+        x: 0,
+        y: 0
+      },
+      val: "ff"
+    };
 
+    for (let i in colors) {
+      grad.addColorStop(i*0.98 + 0.01, colors[i]);
+    }
+    canvas.ctx.fillStyle = grad;
+    canvas.ctx.fillRect(0, 0, w, h);
+    return canvas;
+  }
+  bindListeners(callback, redCallback=null, greenCallback=null, blueCallback=null) {
+    const bars = [
+      [this.red, redCallback],
+      [this.green, greenCallback],
+      [this.blue, blueCallback]
+    ];
+
+    const max = Math.max.bind(Math),
+          min = Math.min.bind(Math);
+
+    const toHex = n => {
+      let num = n.toString(16);
+      if (num.length === 1) {
+        num = "0" + num;
+      }
+      return num;
+    }
+
+    const getVal = (x, y, bar, i) => {
+      const imageData = bar.ctx.getImageData(x, y, 1, 1).data;
+      bar.val = toHex(imageData[i]);
+      return bar.val;
+    }
+
+    const getColor = () => {
+      let color = "#";
+      for (let bar of bars) {
+        color += bar[0].val;
+      }
+      return color;
+    }
+
+    const getPosX = (x, canvas) => {
+      let current = canvas, totalOffset = 0;
+      while (!!current.parentElement) {
+        totalOffset += current.offsetLeft - current.scrollLeft;
+        current = current.parentElement;
+      }
+      return min(max(0, x - totalOffset), this.width - 1);
+    }
+
+    const getPosY = (y, canvas) => {
+      let current = canvas, totalOffset = 0;
+      while (!!current.parentElement) {
+        totalOffset += current.offsetTop - current.scrollTop;
+        current = current.parentElement;
+      }
+      return min(max(0, y - totalOffset), this.height - 1);
+    }
+
+    const createListeners = index => {
+      const [bar, barCallback] = bars[index];
+      const init = e => {
+        const x = getPosX(e.pageX, bar.elem),
+              y = getPosY(e.pageY, bar.elem);
+        bar.pos.x = x;
+        bar.pos.y = y;
+        barCallback(getVal(x, y, bar, index));
+        callback(getColor());
+        window.addEventListener("mousemove", drag);
+        window.addEventListener("mouseup", end);
+      }
+
+      const drag = e => {
+        const x = getPosX(e.pageX, bar.elem),
+              y = getPosY(e.pageY, bar.elem);
+        bar.pos.x = x;
+        bar.pos.y = y;
+        barCallback(getVal(x, y, bar, index));
+        callback(getColor());
+        getColor();
+      }
+
+      const end = () => {
+        window.removeEventListener("mousemove", drag);
+        window.removeEventListener("mouseup", end);
+      }
+
+      bar.elem.addEventListener("mousedown", init);
+    }
+
+    for (let i in bars) {
+      createListeners(i);
+    }
   }
 }
